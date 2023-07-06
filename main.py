30a4d87233ac2ae3c07cd7b9674ee164ec3b46ad
yasuobgg <yasuobgg@gmail.com>
2023-07-06 11:39:41 +0700

    use elephantsql instead

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://mzfogkrw:B5omR4_bKHfaSf4RKabl9kwOJyFzkOX6@john.db.elephantsql.com/mzfogkrw"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
